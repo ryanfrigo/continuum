492e16ba2dcd3d3da013ae5681449b749ff4900a
Ryan Frigo <rfrigo42@gmail.com>
2025-10-06 15:57:19 -0500

    Initial Commit

---
+++ continuum/continuumApp.swift
@@ -0,0 +1,17 @@
+//
+//  continuumApp.swift
+//  continuum
+//
+//  Created by Ryan Frigo on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct continuumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ continuumTests/continuumTests.swift
@@ -0,0 +1,16 @@
+//
+//  continuumTests.swift
+//  continuumTests
+//
+//  Created by Ryan Frigo on 10/6/25.
+//
+
+import Testing
+
+struct continuumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
